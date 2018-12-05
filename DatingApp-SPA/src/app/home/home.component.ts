08e43d2c0a55b66388ee7efe683a2fb86f3ebf5c
Bruce Masenya <bmasenya@iqbusiness.net>
2018-12-05 21:02:39 +0200

    End of Section 4

---
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+
+  constructor(private http: HttpClient) {}
+  values: any;
+  url: string;
+
+  ngOnInit() {
+    this.url = "http://localhost:5000/api/values";
+    this.getValues();
+  }
+
+  registerToggle() {
+    this.registerMode = true;
+  }
+
+  getValues() {
+    // observarable stream of data
+    this.http.get(this.url).subscribe(
+      response => {
+        this.values = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
+}
