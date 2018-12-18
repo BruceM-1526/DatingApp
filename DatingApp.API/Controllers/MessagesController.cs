1d645d0c4d0386582fc912c6b2ae24f3c53daeb2
Bruce Masenya <bmasenya@iqbusiness.net>
2018-12-18 21:27:21 +0200

    End of Section 18

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -12,123 +12,123 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers {
 
-    [ServiceFilter (typeof (LogUserActivity))]
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
-    [Route ("api/users/{userId}/[controller]")]
+    [Route("api/users/{userId}/[controller]")]
     [ApiController]
     public class MessagesController : ControllerBase {
         private readonly IDatingRepository _repo;
         private readonly IMapper _mapper;
 
-        public MessagesController (IDatingRepository repo, IMapper mapper) {
+        public MessagesController(IDatingRepository repo, IMapper mapper) {
             _repo = repo;
             _mapper = mapper;
         }
 
-        [HttpGet ("{id}", Name = "GetMessage")]
-        public async Task<IActionResult> GetMessage (int userId, int id) {
-            if (id != int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value))
-                return Unauthorized ();
+        [HttpGet("{id}", Name = "GetMessage")]
+        public async Task<IActionResult> GetMessage(int userId, int id) {
+            if(id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
 
-            var messageFromRepo = await _repo.GetMessage (id);
+            var messageFromRepo = await _repo.GetMessage(id);
 
-            if (messageFromRepo == null)
-                return NotFound ();
+            if(messageFromRepo == null)
+                return NotFound();
 
-            return Ok (messageFromRepo);
+            return Ok(messageFromRepo);
 
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetMessagesForUser (int userId, [FromQuery] MessageParams messageParams) {
-            if (userId != int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value))
-                return Unauthorized ();
+        public async Task<IActionResult> GetMessagesForUser(int userId, [FromQuery] MessageParams messageParams) {
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
 
             messageParams.UserId = userId;
 
-            var messagesFromRepo = await _repo.GetMessagesForUser (messageParams);
+            var messagesFromRepo = await _repo.GetMessagesForUser(messageParams);
 
-            var messages = _mapper.Map<IEnumerable<MessageToReturnDto>> (messagesFromRepo);
+            var messages = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
 
-            Response.AddPagination (messagesFromRepo.CurrentPage,
+            Response.AddPagination(messagesFromRepo.CurrentPage,
                 messagesFromRepo.PageSize, messagesFromRepo.TotalCount,
                 messagesFromRepo.TotalPages);
 
-            return Ok (messages);
+            return Ok(messages);
 
         }
 
-        [HttpGet ("thread/{recipientId}")]
-        public async Task<IActionResult> GetMessageThread (int userId, int recipientId) {
-            if (userId != int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value))
-                return Unauthorized ();
+        [HttpGet("thread/{recipientId}")]
+        public async Task<IActionResult> GetMessageThread(int userId, int recipientId) {
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
 
-            var messageFromRepo = await _repo.GetMessageThread (userId, recipientId);
+            var messageFromRepo = await _repo.GetMessageThread(userId, recipientId);
 
-            var messageThread = _mapper.Map<IEnumerable<MessageToReturnDto>> (messageFromRepo);
+            var messageThread = _mapper.Map<IEnumerable<MessageToReturnDto>>(messageFromRepo);
 
-            return Ok (messageThread);
+            return Ok(messageThread);
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateMessage (int userId, MessageForCreationDto messageForCreationDto) {
-            var sender = await _repo.GetUser (userId);
+        public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto) {
+            var sender = await _repo.GetUser(userId);
 
-            if (sender.Id != int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value))
-                return Unauthorized ();
+            if(sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
 
             messageForCreationDto.SenderId = userId;
 
-            var recipient = await _repo.GetUser (messageForCreationDto.RecipientId);
-            if (recipient == null)
-                return BadRequest ("Could not find user");
+            var recipient = await _repo.GetUser(messageForCreationDto.RecipientId);
+            if(recipient == null)
+                return BadRequest("Could not find user");
 
-            var message = _mapper.Map<Message> (messageForCreationDto);
+            var message = _mapper.Map<Message>(messageForCreationDto);
 
-            _repo.Add (message);
+            _repo.Add(message);
 
             // var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
 
-            if (await _repo.SaveAll ()) {
-                var messageToReturn = _mapper.Map<MessageToReturnDto> (message);
-                return CreatedAtRoute ("GetMessage", new { id = message.Id }, messageToReturn);
+            if(await _repo.SaveAll()) {
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
+                return CreatedAtRoute("GetMessage", new { id = message.Id }, messageToReturn);
             }
 
-            throw new Exception ("Creating the message failed on save");
+            throw new Exception("Creating the message failed on save");
         }
 
-        [HttpPost ("{id}")]
-        public async Task<IActionResult> DeleteMessage (int id, int userId) {
-            if (userId != int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value))
-                return Unauthorized ();
+        [HttpPost("{id}")]
+        public async Task<IActionResult> DeleteMessage(int id, int userId) {
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
 
-            var messageFromRepo = await _repo.GetMessage (id);
+            var messageFromRepo = await _repo.GetMessage(id);
 
-            if (messageFromRepo.SenderId == userId)
+            if(messageFromRepo.SenderId == userId)
                 messageFromRepo.SenderDeleted = true;
 
-            if (messageFromRepo.RecipientId == userId)
+            if(messageFromRepo.RecipientId == userId)
                 messageFromRepo.RecipientDeleted = true;
 
-            if (messageFromRepo.SenderDeleted && messageFromRepo.RecipientDeleted)
-                _repo.Delete (messageFromRepo);
+            if(messageFromRepo.SenderDeleted && messageFromRepo.RecipientDeleted)
+                _repo.Delete(messageFromRepo);
 
-            if (await _repo.SaveAll ())
-                return NoContent ();
+            if(await _repo.SaveAll())
+                return NoContent();
 
-            throw new Exception ("Error deleting the message");
+            throw new Exception("Error deleting the message");
 
         }
 
-        [HttpPost ("{id}/read")]
-        public async Task<IActionResult> MarkMessageAsRead (int id, int userId) {
-            if (userId != int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value))
-                return Unauthorized ();
+        [HttpPost("{id}/read")]
+        public async Task<IActionResult> MarkMessageAsRead(int id, int userId) {
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
 
-            var message = await _repo.GetMessage (id);
+            var message = await _repo.GetMessage(id);
 
-            if (message.RecipientId != userId)
-              return Unauthorized ();
+            if(message.RecipientId != userId)
+              return Unauthorized();
 
             message.IsRead = true;
             message.DateRead = DateTime.Now;
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,37 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Helpers;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
-namespace DatingApp.API {
+namespace DatingApp.API
+{
     public class Startup {
         public Startup(IConfiguration configuration) {
             Configuration = configuration;
+        }
+
+        public enum CoreEventId 
+        {
+
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(x => x
+            .UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                 .AddJsonOptions(opt => {
                     opt.SerializerSettings.ReferenceLoopHandling =
@@ -75,7 +75,14 @@ namespace DatingApp.API {
             // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "Index"}
+                );
+            });
         }
     }
 }
